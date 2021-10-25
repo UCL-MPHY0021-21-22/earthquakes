d27d2516390930c89b0647f03dbedebef6da9c55
Pete Jones <peterjones252@gmail.com>
2021-10-25 11:22:11 +0100

    Adds functions to find strongest earthquake

--- earthquakes.py
+++ earthquakes.py
@@ -1,13 +1,7 @@
-# The Python standard library includes some functionality for communicating
-# over the Internet.
-# However, we will use a more powerful and simpler library called requests.
-# This is external library that you may need to install first.
 import requests
-
+import json
 
 def get_data():
-    # With requests, we can ask the web service for the data.
-    # Can you understand the parameters we are passing here?
     response = requests.get(
         "http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
         params={
@@ -20,38 +14,32 @@ def get_data():
             "endtime": "2018-10-11",
             "orderby": "time-asc"}
     )
-
-    # The response we get back is an object with several fields.
-    # The actual contents we care about are in its text field:
     text = response.text
-    # To understand the structure of this text, you may want to save it
-    # to a file and open it in VS Code or a browser.
-    # See the README file for more information.
-    ...
-
-    # We need to interpret the text to get values that we can work with.
-    # What format is the text in? How can we load the values?
-    return ...
+    data = json.loads(text)
+    return data["features"]
 
 def count_earthquakes(data):
     """Get the total number of earthquakes in the response."""
-    return ...
+    return len(data)
 
 
 def get_magnitude(earthquake):
     """Retrive the magnitude of an earthquake item."""
-    return ...
+    return earthquake["properties"]["mag"]
 
 
 def get_location(earthquake):
     """Retrieve the latitude and longitude of an earthquake item."""
-    # There are three coordinates, but we don't care about the third (altitude)
-    return ...
+    return earthquake["geometry"]["coordinates"][0:2]
 
 
 def get_maximum(data):
     """Get the magnitude and location of the strongest earthquake in the data."""
-    ...
+    mags = [get_magnitude(quake) for quake in data]
+    max_value = max(mags)
+    index = mags.index(max_value)
+    return [max_value, get_location(data[index])]
+
 
 
 # With all the above functions defined, we can now call them and get the result
