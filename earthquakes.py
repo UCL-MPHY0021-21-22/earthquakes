6acf447a007c13c8a4b75cff98d1dc0249a65078
YinwenYang <yangyinwen99@163.com>
2021-10-28 22:22:59 +0800

    working with earthquake data set

--- earthquakes.py
+++ earthquakes.py
@@ -3,7 +3,7 @@
 # However, we will use a more powerful and simpler library called requests.
 # This is external library that you may need to install first.
 import requests
-
+import json
 
 def get_data():
     # With requests, we can ask the web service for the data.
@@ -27,32 +27,42 @@ def get_data():
     # To understand the structure of this text, you may want to save it
     # to a file and open it in VS Code or a browser.
     # See the README file for more information.
+
+    textData = json.loads(text)
     ...
+    with open('earthquake.json','w') as data_file:
+        data_file.write(json.dumps(textData))
 
     # We need to interpret the text to get values that we can work with.
     # What format is the text in? How can we load the values?
-    return ...
+    return textData
+    
+
 
 def count_earthquakes(data):
     """Get the total number of earthquakes in the response."""
-    return ...
+    return data['metadata']['count']
 
 
 def get_magnitude(earthquake):
     """Retrive the magnitude of an earthquake item."""
-    return ...
+    return earthquake['properties']['mag']
 
 
 def get_location(earthquake):
     """Retrieve the latitude and longitude of an earthquake item."""
     # There are three coordinates, but we don't care about the third (altitude)
-    return ...
+    return earthquake['geometry']['coordinates'][:-1]
 
 
 def get_maximum(data):
     """Get the magnitude and location of the strongest earthquake in the data."""
     ...
-
+    max_earthquake = data['features'][0]
+    for earthquake in data['features']:
+        if (get_magnitude(earthquake) > get_magnitude(max_earthquake)):
+            max_earthquake = earthquake
+    return  get_magnitude(max_earthquake),get_location(max_earthquake)
 
 # With all the above functions defined, we can now call them and get the result
 data = get_data()
